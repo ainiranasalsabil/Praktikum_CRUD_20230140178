ef4a3fbfd8bfe56293b3cea021ff35dfbf2b2fdb
ainiranasalsabil <ainiranasalsabil04@gmail.com>
2026-03-03 12:48:40 +0700

    menambahkan UserMapper untuk konversi User ke UserDto

--- src/main/java/com/example/praktikum1/mapper/UserMapper.java
+++ src/main/java/com/example/praktikum1/mapper/UserMapper.java
@@ -8,6 +8,8 @@ import org.mapstruct.factory.Mappers;
 
 @Mapper
 public interface UserMapper {
+
     UserMapper MAPPER = Mappers.getMapper(UserMapper.class);
-    UserDto toUserDtoData= (User user);
+
+    UserDto toUserDtoData(User user);
 }
